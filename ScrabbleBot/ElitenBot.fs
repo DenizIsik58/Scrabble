8d25c039dcf0aa96df79c50f10e349221bd8d6e2
Deniz Isik <denizisik4600@gmail.com>
2022-05-03 21:41:24 +0200

    Add some comments

--- ScrabbleBot/ElitenBot.fs
+++ ScrabbleBot/ElitenBot.fs
@@ -14,7 +14,10 @@ module internal ElitenBot =
     let intToChar (toAsci : uint32) = Convert.ToChar (toAsci + 64u)
     let move (piecesOnBoard:Map<coord, piece>) (hand: MultiSet.MultiSet<uint32>) (pieces:uint32 * uint32) list =
         let wordsInHand = hand |> Map.fold (fun acc key value -> (intToChar key)::acc) [] |> List.toArray |> String
-
+        // Take the string and bruteforce till you find a word in the dictionary if you are the first one to play
+        // Otherwise run through the tiles on the board and see if you can place any of chars in your hand by using the step function
+        
+        
         for entry in piecesOnBoard do
             let x, y = entry.Key
             let pId, (ch, pv) = entry.Value
